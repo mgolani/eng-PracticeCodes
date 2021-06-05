46bc024de294684a1d1d13846607614c27e08ed2
Mitul Golani <mituln.golani@gmail.com>
2021-06-06 00:10:32 +0530

    Remove Unnecessary a.outs

--- StringCodeAssignments/5exe.c
+++ StringCodeAssignments/5exe.c
@@ -1,36 +1,31 @@
-//Write Program to compare two strings without strcmp function
-//int strcmp(const char *s1, const char *s2);
+//Write a program to compare two strings without using strcmp function.
 #include <stdio.h>
 #include <stdlib.h>
-int
-StrCmp (char *Str1, char *Str2) {
-    while(*Str1!='\0') {
-      if(*Str1!=*Str2)
-        break;
-    
-      Str1++;
-      Str2++;
-    }
-    if(*Str1==*Str2){
-        printf("Both are same\n");
-        return 0;
-    } else {
-        printf("Both are not same\n");
-        return -1;
-    }
-}
+int main () {
 
-int
-main() {
-  char *Str1=malloc(10);
-  char *Str2=malloc(10);
-  int  Status;
+	#define STRING_SIZE 20
+	char *Str1, *Str2;
+	int i=0;
 
-  printf("Please enter String-1\n");
-  scanf("%s", Str1);
-  printf("Please enter String-2\n");
-  scanf("%s", Str2);
+	Str1 = (char *)malloc(STRING_SIZE);
+	Str2 = (char *)malloc(STRING_SIZE);
+	printf("Enter String:1\t");
+	scanf("%s", Str1);
 
-  Status=StrCmp(Str1,Str2);
-  return Status;
+        printf("\nEnter String:2\t");
+        scanf("%s", Str2);
+
+	for(i=0;Str1[i];i++) {
+		if(Str1[i]!=Str2[i])
+			break;
+		else
+			continue;
+	}
+
+	if(Str1[i]==Str2[i])
+		printf ("Same\n");
+	else
+		printf("Not Same\n");
+	
+	return 0;
 }
