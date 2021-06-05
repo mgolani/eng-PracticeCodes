2b4cc38595e0b73d39003f6175cbb8b1a32ba369
Anupam <anupam.km@pathpartnertech.com>
2021-06-05 22:57:25 +0530

    intilizated string buffer before use as string pointer was giving seg fault, If using string pointer dynamically allocate memory before use

--- StringCodeAssignments/7exe.c
+++ StringCodeAssignments/7exe.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 int main() {
 
-  char *Str;
+  char Str[20];
   int Length=0, i, j; 
   //Take Input from user
   printf("Please Enter String\n");
   scanf("%s",Str);
 
   //Length of string
-  for(Length=0; Str[Length]!='\0'; Length++);
+  for(Length=0; Str[Length]; Length++);
 
   //Display Entered Input
   printf("Entered Input is:%s and length %d\n",Str, Length);
